f1b8640803115cee124b96b19001ed92ff9dad4e
anri001 <andria.razmadze@yahoo.com>
2024-07-10 14:17:53 +0400

    first commit

---
+++ day 5/homeworkp2.py
@@ -0,0 +1,24 @@
+print("what's your name")
+name = input()
+print("what's your surname")
+surname = input()
+print("what's your age")
+age = input()
+
+
+print(name, surname, "is", age, "years old")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
