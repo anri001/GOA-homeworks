8d0f3feb7ff0e84a2607deb71f841759b0d6beda
anri001 <andria.razmadze@yahoo.com>
2024-09-07 20:21:02 +0400

    second commit

---
+++ day 22/homework/homework.py
@@ -0,0 +1,7 @@
+name = input("first name :")
+surname = input("last name :")
+password = input("password :")
+repeat_password = input("repeat password :")
+while repeat_password != password:
+    repeat_password = input("repeat password :")
+email = input("emali :")
